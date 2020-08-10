8d7e72df54d9bfc72a4a8d14596a88fdf1f7e8dd
wt <306859314@qq.com>
2020-08-10 11:33:06 +0800

    Update main.js
    修复bug，只支持wlan

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const QRCode = require('qrcode')
 const app = express();
 const os = require('os');
-const fs = require('fs')
+const fs = require('fs');
 
 app.use(express.static('public'))
 app.use('/public', express.static('public'))
@@ -10,6 +10,7 @@ app.listen(9090)
 
 fs.readdir('./public', (err, files) => {
     files.forEach(function (item, index, array) {
+        // console.log(TotalIp + item);
         QRCode.toString(TotalIp + item, { type: 'terminal', errorCorrectionLevel: ' L ', version: 3 }, function (err, url) {
             console.log(url)
         })
@@ -19,7 +20,10 @@ fs.readdir('./public', (err, files) => {
 
 function getIPAdress() {
     var interfaces = os.networkInterfaces();
+    // console.log(interfaces);
     for (var devName in interfaces) {
+        // console.log(devName);
+        if(devName!='WLAN')continue
         var iface = interfaces[devName];
         for (var i = 0; i < iface.length; i++) {
             var alias = iface[i];
@@ -30,4 +34,5 @@ function getIPAdress() {
     }
 }
 const Ip = getIPAdress();
-const TotalIp = "http://" + Ip + ":9090/static/"
+const TotalIp = "http://" + Ip + ":9090/public/"
+// console.log(Ip);

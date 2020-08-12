55224bbef9cf9edd1393610556046f6a75a3fff3
wt <306859314@qq.com>
2020-08-12 17:11:36 +0800

    从终端打印二维码改为直接打开二维码图片

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # qrcp_node
  尝试用node实现类似qrcp的功能
+
+ 暂时只支持wlan网络
--- main.js
+++ main.js
@@ -3,19 +3,31 @@ const QRCode = require('qrcode')
 const app = express();
 const os = require('os');
 const fs = require('fs');
+// const child_process=require('child_process')
+const open = require('open');
 
 app.use(express.static('public'))
 app.use('/public', express.static('public'))
 app.listen(9090)
 
+var opts={
+    errorCorrectionLevel: 'H',
+    type: 'image/jpeg',
+    quality: 0.3,
+    margin: 1,
+    color: {
+        dark:"#FFFFFF",
+        light:"#000000"
+    }
+}
+var targetPath='./target/'
 fs.readdir('./public', (err, files) => {
     files.forEach(function (item, index, array) {
-        // console.log(TotalIp + item);
-        QRCode.toString(TotalIp + item, { type: 'terminal', errorCorrectionLevel: ' L ', version: 3 }, function (err, url) {
-            console.log(url)
+        QRCode.toFile(targetPath+item.replace(/\.[^\.]+$/,'.png'),TotalIp + item, opts,(err)=>{
+            var path=targetPath+item.replace(/\.[^\.]+$/,'.png');
+            open(path)
         })
     });
-
 })
 
 function getIPAdress() {
